b547473f71db7e9b4cbc5d52894d0363f8d6770f
Ricardo M. de Oliveira <ricardo.martinelli.oliveira@gmail.com>
2015-02-01 23:00:57 -0200

    Initial version of the page

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,34 @@
 var express = require('express');
 var router = express.Router();
 
+var robot = require("raspirobot");
+robot.setup();
+
+robot.setup();
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/forward', function(req, res, next) {
+	robot.setMotor("left", 1, 1);
+	robot.setMotor("right", 1, 1);
+});
+
+router.get('/left', function(req, res, next) {
+	robot.setMotor("left", 1, 0);
+	robot.setMotor("right", 1, 1);
+});
+
+router.get('/down', function(req, res, next) {
+	robot.setMotor("left", 1, 1);
+	robot.setMotor("right", 1, 0);
+});
+
+router.get('/backward', function(req, res, next) {
+	robot.setMotor("left", 1, 0);
+	robot.setMotor("right", 1, 0);
 });
 
 module.exports = router;
