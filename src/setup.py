8ee3a32c62e07a85f742e3bb4822eddfa5306c53
Ricardo M. Oliveira <ricardo.martinelli.oliveira@gmail.com>
2015-09-07 23:34:41 -0300

    Rewriting from scratch

---
+++ src/setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+""" Robot Raspberry Pi setup module """
+
+from setuptools import setup
+
+pkgname = "robot-raspberrypi"
+version = "0.0.1"
+
+
+
+metadata = {
+    "name": pkgname,
+    "version": version,
+    "author": "Ricardo M. Oliveira",
+    "author_email": "",
+    "url": "http://github.com/robotica-livre/robot-raspberrypi",
+    "description": ""
+}
+
+
+
+
+setup(**metadata)
